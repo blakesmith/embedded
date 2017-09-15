ad0a963a3aa08766553f09b803d6d5d49802ab30
Blake Smith <blakesmith0@gmail.com>
2017-09-14 20:25:27 -0500

    Make rotary encoder work

--- drivers/src/pec11_renc.cc
+++ drivers/src/pec11_renc.cc
@@ -2,21 +2,26 @@
 
 #include "pec11_renc.h"
 
-#define CLOCKWISE_PIN GPIO_Pin_1
-#define CLOCKWISE_PINSOURCE EXTI_PinSource1
-#define CLOCKWISE_IDR GPIO_IDR_IDR_1
+#define CLOCKWISE_PIN GPIO_Pin_2
+#define CLOCKWISE_PINSOURCE EXTI_PinSource2
+#define CLOCKWISE_IDR GPIO_IDR_IDR_2
+#define CLOCKWISE_GPIO GPIOE
+#define EXTI_CLOCKWISE_PORTSOURCE EXTI_PortSourceGPIOE
+#define CLOCKWISE_EXTI_LINE EXTI_Line2
+#define CLOCKWISE_NVIC_IRQ_CHANNEL EXTI2_IRQn
 
-#define COUNTER_CLOCKWISE_PIN GPIO_Pin_2
-#define COUNTER_CLOCKWISE_PINSOURCE EXTI_PinSource2
-#define COUNTER_CLOCKWISE_IDR GPIO_IDR_IDR_2
+#define COUNTER_CLOCKWISE_PIN GPIO_Pin_5
+#define COUNTER_CLOCKWISE_PINSOURCE EXTI_PinSource5
+#define COUNTER_CLOCKWISE_IDR GPIO_IDR_IDR_5
+#define COUNTER_CLOCKWISE_GPIO GPIOE
+#define COUNTER_CLOCKWISE_EXTI_LINE EXTI_Line5
+#define EXTI_COUNTER_CLOCKWISE_PORTSOURCE EXTI_PortSourceGPIOE
+#define COUNTER_CLOCKWISE_NVIC_IRQ_CHANNEL EXTI9_5_IRQn
 
 #define BUTTON_PIN GPIO_Pin_3
 #define BUTTON_PINSOURCE EXTI_PinSource3
 #define BUTTON_IDR GPIO_IDR_IDR_3
 
-#define GPIOx GPIOA
-#define EXTI_PORTSOURCE EXTI_PortSourceGPIOA
-#define EXTI_LINE EXTI_Line0
 
 Pec11RotaryEncoder::Pec11RotaryEncoder() :
     cw_pin_state_(read_pin_state(CLOCKWISE_IDR)),
@@ -28,27 +33,36 @@ void Pec11RotaryEncoder::Init() {
     EXTI_InitTypeDef encoder_exti;
     NVIC_InitTypeDef encoder_nvic;
     
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE, ENABLE);
     RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
-    
-    encoder_gpio.GPIO_Pin = CLOCKWISE_PIN | COUNTER_CLOCKWISE_PIN | BUTTON_PIN;
+
+    GPIO_StructInit(&encoder_gpio);
+    encoder_gpio.GPIO_Pin = CLOCKWISE_PIN;
     encoder_gpio.GPIO_Mode = GPIO_Mode_IN;
     encoder_gpio.GPIO_PuPd = GPIO_PuPd_UP;
     encoder_gpio.GPIO_OType = GPIO_OType_PP;
-    encoder_gpio.GPIO_Speed = GPIO_Speed_2MHz;
-    GPIO_Init(GPIOx, &encoder_gpio);
+    encoder_gpio.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(CLOCKWISE_GPIO, &encoder_gpio);
 
-    SYSCFG_EXTILineConfig(EXTI_PORTSOURCE, CLOCKWISE_PINSOURCE);
-    SYSCFG_EXTILineConfig(EXTI_PORTSOURCE, COUNTER_CLOCKWISE_PINSOURCE);
-    SYSCFG_EXTILineConfig(EXTI_PORTSOURCE, BUTTON_PINSOURCE);
+    // GPIO_StructInit(&encoder_gpio);
+    // encoder_gpio.GPIO_Pin = COUNTER_CLOCKWISE_PIN;
+    // encoder_gpio.GPIO_Mode = GPIO_Mode_IN;
+    // encoder_gpio.GPIO_PuPd = GPIO_PuPd_UP;
+    // encoder_gpio.GPIO_OType = GPIO_OType_PP;
+    // encoder_gpio.GPIO_Speed = GPIO_Speed_50MHz;
+    // GPIO_Init(COUNTER_CLOCKWISE_GPIO, &encoder_gpio);
 
-    encoder_exti.EXTI_Line = EXTI_LINE;
+    SYSCFG_EXTILineConfig(EXTI_CLOCKWISE_PORTSOURCE, CLOCKWISE_PINSOURCE);
+//    SYSCFG_EXTILineConfig(EXTI_COUNTER_CLOCKWISE_PORTSOURCE, COUNTER_CLOCKWISE_PINSOURCE);
+//    SYSCFG_EXTILineConfig(EXTI_PORTSOURCE, BUTTON_PINSOURCE);
+
+    encoder_exti.EXTI_Line = CLOCKWISE_EXTI_LINE;
     encoder_exti.EXTI_LineCmd = ENABLE;
     encoder_exti.EXTI_Mode = EXTI_Mode_Interrupt;
     encoder_exti.EXTI_Trigger = EXTI_Trigger_Rising_Falling;
     EXTI_Init(&encoder_exti);
 
-    encoder_nvic.NVIC_IRQChannel = EXTI0_IRQn;
+    encoder_nvic.NVIC_IRQChannel = CLOCKWISE_NVIC_IRQ_CHANNEL;
     encoder_nvic.NVIC_IRQChannelPreemptionPriority = 0;
     encoder_nvic.NVIC_IRQChannelSubPriority = 0;
     encoder_nvic.NVIC_IRQChannelCmd = ENABLE;
@@ -59,7 +73,7 @@ EncoderAction Pec11RotaryEncoder::GetAction() {
     PinState ps;
     EncoderAction action;
     
-    if (EXTI_GetITStatus(EXTI_LINE) != RESET) {
+    if (EXTI_GetITStatus(CLOCKWISE_EXTI_LINE) != RESET) {
         ps = read_pin_state(CLOCKWISE_IDR);
         if (ps != cw_pin_state_) {
             cw_pin_state_ = ps;
@@ -76,7 +90,7 @@ EncoderAction Pec11RotaryEncoder::GetAction() {
             action = ENC_ACTION_PUSH_BUTTON;
         }
         
-        EXTI_ClearITPendingBit(EXTI_LINE);
+        EXTI_ClearITPendingBit(CLOCKWISE_EXTI_LINE);
         return action;
     }
 
@@ -84,7 +98,7 @@ EncoderAction Pec11RotaryEncoder::GetAction() {
 }
 
 PinState Pec11RotaryEncoder::read_pin_state(uint32_t idr) {
-    if (GPIOx->IDR & idr) {
+    if (CLOCKWISE_GPIO->IDR & idr) {
         return PIN_STATE_HIGH;
     } else {
         return PIN_STATE_LOW;
--- test_project/src/main.cc
+++ test_project/src/main.cc
@@ -7,6 +7,7 @@
 GPIO_InitTypeDef GPIO_LED;
 HT16K33Display display;
 Pec11RotaryEncoder knob;
+volatile bool button_pressed = false;
 
 static const uint32_t states[] = {
     GPIO_Pin_12,
@@ -28,18 +29,56 @@ static const uint32_t states[] = {
 static const size_t n_states = sizeof(states) / sizeof(uint32_t);
 static size_t current_state = 0;
 
-void Init() {
+void AdvanceLedState(size_t n);
+
+void InitOnboardLed() {
     RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
+    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
+    
     GPIO_LED.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
     GPIO_LED.GPIO_Mode = GPIO_Mode_OUT;
     GPIO_LED.GPIO_OType = GPIO_OType_PP;
     GPIO_LED.GPIO_Speed = GPIO_Speed_50MHz;
     GPIO_Init(GPIOD, &GPIO_LED);
+}
 
-    display.Init();
+void InitOnboardButton() {
+    GPIO_InitTypeDef button_gpio;
+    EXTI_InitTypeDef button_exti;
+    NVIC_InitTypeDef button_nvic;
+
+    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+    
+    GPIO_StructInit(&button_gpio);
+    button_gpio.GPIO_Pin = GPIO_Pin_0;
+    button_gpio.GPIO_Mode = GPIO_Mode_IN;
+    button_gpio.GPIO_OType = GPIO_OType_PP;
+    button_gpio.GPIO_PuPd = GPIO_PuPd_DOWN;
+    button_gpio.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &button_gpio);
+
+    SYSCFG_EXTILineConfig(EXTI_PortSourceGPIOA, EXTI_PinSource0);
+
+    button_exti.EXTI_Line = EXTI_Line0;
+    button_exti.EXTI_LineCmd = ENABLE;
+    button_exti.EXTI_Mode = EXTI_Mode_Interrupt;
+    button_exti.EXTI_Trigger = EXTI_Trigger_Falling;
+    EXTI_Init(&button_exti);
+
+    button_nvic.NVIC_IRQChannel = EXTI0_IRQn;
+    button_nvic.NVIC_IRQChannelPreemptionPriority = 0;
+    button_nvic.NVIC_IRQChannelSubPriority = 0;
+    button_nvic.NVIC_IRQChannelCmd = ENABLE;
+    NVIC_Init(&button_nvic);
+}
+
+void Init() {
+    InitOnboardLed();
+//    InitOnboardButton();
+//    display.Init();
     knob.Init();
 
-    uint32_t rc = SysTick_Config(SystemCoreClock / 10);
+    uint32_t rc = SysTick_Config(SystemCoreClock / 5);
     if (rc != 0) {
         GPIO_WriteBit(GPIOD, GPIO_Pin_13, Bit_SET);
     }
@@ -64,10 +103,9 @@ void UpdateDisplay() {
 
 extern "C" {
 void SysTick_Handler(void) {
-//    UpdateDisplay();
 }
 
-void EXTI0_IRQHandler() {
+void EXTI2_IRQHandler(void) {
     EncoderAction action = knob.GetAction();
 
     if (action == ENC_ACTION_ROTATE_CLOCKWISE) {
@@ -75,7 +113,7 @@ void EXTI0_IRQHandler() {
     } else if (action == ENC_ACTION_ROTATE_COUNTER_CLOCKWISE) {
         AdvanceLedState(-1);
     } else if (action == ENC_ACTION_PUSH_BUTTON) {
-        AdvanceLedState(0);
+       AdvanceLedState(0);
     }
 }
 }
