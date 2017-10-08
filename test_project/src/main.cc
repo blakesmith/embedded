977cbdb6040263a29a9cbfe557da5b7267f7b943
Blake Smith <blakesmith0@gmail.com>
2017-10-08 08:26:02 -0500

    Use StatusLed in test_project

--- test_project/src/main.cc
+++ test_project/src/main.cc
@@ -4,38 +4,13 @@
 #include "ht16K33_display.h"
 #include "pec11_renc.h"
 #include "cs43l22_dac.h"
+#include "status_led.h"
 
-GPIO_InitTypeDef GPIO_LED;
+StatusLed status_led;
 HT16K33Display display;
 Pec11RotaryEncoder knob;
 
-void SetLedState(long count);
-
 volatile uint32_t count = 0;
-
-static const uint32_t states[] = {
-    0,
-    GPIO_Pin_12,
-    GPIO_Pin_12 | GPIO_Pin_13,
-    GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14,
-    GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15,
-    GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15,
-    GPIO_Pin_14 | GPIO_Pin_15,
-    GPIO_Pin_15,
-};
-
-static const size_t n_states = sizeof(states) / sizeof(uint32_t);
-
-void InitOnboardLed() {
-    RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOD, ENABLE);
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_SYSCFG, ENABLE);
-    
-    GPIO_LED.GPIO_Pin = GPIO_Pin_12 | GPIO_Pin_13 | GPIO_Pin_14 | GPIO_Pin_15;
-    GPIO_LED.GPIO_Mode = GPIO_Mode_OUT;
-    GPIO_LED.GPIO_OType = GPIO_OType_PP;
-    GPIO_LED.GPIO_Speed = GPIO_Speed_50MHz;
-    GPIO_Init(GPIOD, &GPIO_LED);
-}
 
 void InitOnboardButton() {
     GPIO_InitTypeDef button_gpio;
@@ -68,22 +43,15 @@ void InitOnboardButton() {
 }
 
 void Init() {
-    InitOnboardLed();
+    status_led.Init();
 //    InitOnboardButton();
     display.Init();
 //    knob.Init();
 
     uint32_t rc = SysTick_Config(SystemCoreClock / 10);
     if (rc != 0) {
-        GPIO_WriteBit(GPIOD, GPIO_Pin_13, Bit_SET);
+        status_led.SetError(true);
     }
-
-    SetLedState(3);
-}
-
-void SetLedState(long count) {
-    uint32_t pins = states[count % n_states];
-    GPIOD->ODR = pins;
 }
 
 void UpdateDisplay() {
@@ -118,5 +86,6 @@ int main() {
     Init();
 
     UpdateDisplay();
+    status_led.SetOk(true);
     while (true);
 }
