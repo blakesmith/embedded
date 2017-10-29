99d0115aeff7455dcf653617e796820fe174881e
Blake Smith <blakesmith0@gmail.com>
2017-10-29 17:27:59 -0500

    Make GPIOBusses on each peripheral configurable

--- nome/src/pinout.cc
+++ nome/src/pinout.cc
@@ -1,24 +1,33 @@
 #include "pinout.h"
 
+// Begin configuration
+
 // StatusLed
 static constexpr uint16_t STATUS_OK_LED = 15;
 static constexpr uint16_t STATUS_ERROR_LED = 14;
 static constexpr uint16_t STATUS_ACTIVITY_LED = 13;
+
+#define STATUS_LED_BUS gpiod
 
 // I2C
 static constexpr uint16_t I2C_SCL = 8;
 static constexpr uint16_t I2C_SDA = 9;
 static constexpr I2CBus::Id I2C_ID = I2CBus::Id::THREE;
 
+#define I2C_SCL_BUS gpioa
+#define I2C_SDA_BUS gpioc
+
+// End configuration
+
 Pinout::Pinout()
     : gpioa(GPIOBus::Id::A),
       gpioc(GPIOBus::Id::C),
       gpiod(GPIOBus::Id::D),
-      led_ok(gpiod, STATUS_OK_LED),
-      led_error(gpiod, STATUS_ERROR_LED),
-      led_activity(gpiod, STATUS_ACTIVITY_LED),
-      i2c_scl(gpioa, I2C_SCL),
-      i2c_sda(gpioc, I2C_SDA),
+      led_ok(STATUS_LED_BUS, STATUS_OK_LED),
+      led_error(STATUS_LED_BUS, STATUS_ERROR_LED),
+      led_activity(STATUS_LED_BUS, STATUS_ACTIVITY_LED),
+      i2c_scl(I2C_SCL_BUS, I2C_SCL),
+      i2c_sda(I2C_SDA_BUS, I2C_SDA),
       i2c_bus(I2C_ID, i2c_scl, i2c_sda)
 {}
 
