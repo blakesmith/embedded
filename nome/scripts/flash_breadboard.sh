453a0fe008d1f0b01e4adce786845bcf9777f683
Blake Smith <blakesmith0@gmail.com>
2017-11-02 00:08:38 -0500

    Add flash_breadboard script

---
+++ nome/scripts/flash_breadboard.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+OPENOCD=openocd
+OPENOCD_CONFIG=/usr/share/openocd/scripts/board/stm32f4discovery.cfg
+HEX=../../bazel-out/clang_linux_arm-fastbuild/bin/nome/nome_breadboard.hex
+
+$OPENOCD -f $OPENOCD_CONFIG \
+         -c "init" \
+         -c "reset halt" \
+         -c "flash write_image erase $HEX" \
+         -c "reset run" \
+         -c "exit"
+
