3c6d8f60abfcb49ba2cbcef3fe72f008a5d205dc
Blake Smith <blakesmith0@gmail.com>
2018-07-09 15:24:12 -0500

    Implement basic scanning and status output in main.

--- keebee/src/main.cc
+++ keebee/src/main.cc
@@ -1,5 +1,6 @@
 #include "drivers/stm32/gpio_bus.h"
 #include "drivers/stm32/scan_matrix.h"
+#include "drivers/stm32/status_led.h"
 
 using namespace stm32;
 
@@ -14,23 +15,52 @@ GPIOPin scan_rows[] = {
 GPIOPin scan_columns[] = {
     GPIOPin(gpioa, 0),
     GPIOPin(gpioa, 1),
-    GPIOPin(gpioa, 2),
-    GPIOPin(gpioa, 3),
 };
+
+const uint8_t row_count = sizeof(scan_rows) / sizeof(GPIOPin);
+const uint8_t column_count = sizeof(scan_columns) / sizeof(GPIOPin);
+
+// Make sure to update this with the number of columns
+bool *key_scans[row_count] = {
+    {},
+    {}
+};
+
+GPIOPin st1_ok(gpioa, 7);
+GPIOPin st1_err(gpioa, 8);
+GPIOPin st2_ok(gpioa, 9);
+GPIOPin st2_err(gpioa, 10);
+
+StatusLed st1(st1_ok, st1_err);
+StatusLed st2(st2_ok, st2_err);
 
 ScanMatrix scan_matrix(scan_rows,
                        scan_columns,
-                       sizeof(scan_rows) / sizeof(GPIOPin),
-                       sizeof(scan_columns) / sizeof(GPIOPin));
+                       row_count,
+                       column_count);
 
 static void Init() {
+    gpioa.Init();
+    gpiob.Init();
+    st1.Init();
+    st2.Init();
     scan_matrix.Init();
+}
+
+static void scan_and_update() {
+    scan_matrix.Scan(key_scans, row_count, column_count);
+    st1.SetOk(key_scans[0][0]);
+    st1.SetError(key_scans[0][1]);
+    st2.SetOk(key_scans[1][0]);
+    st2.SetError(key_scans[1][1]);
 }
 
 int main() {
     Init();
 
-    while (true);
+    while (true) {
+        scan_and_update();
+    }
 }
 
 extern "C" {
