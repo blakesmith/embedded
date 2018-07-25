2aeeec9c02415e4e446e19a9dfc5c18f5196211f
Blake Smith <blakesmith0@gmail.com>
2018-07-24 20:57:54 -0500

    Fill in untested dip switch init and read.

--- drivers/stm32/dip_switch.cc
+++ drivers/stm32/dip_switch.cc
@@ -7,10 +7,29 @@ DipSwitch::DipSwitch(GPIOPin* pins, const uint8_t pin_count)
       pin_count_(pin_count) { }
 
 void DipSwitch::Init() {
+    for (unsigned int i = 0; i < pin_count_; i++) {
+        GPIOPin& pin = pins_[i];
+
+        pin.set_mode(GPIOPin::Mode::IN);
+        pin.set_pupd(GPIOPin::PuPd::UP);
+        pin.set_output(GPIOPin::OType::PUSH_PULL);
+        pin.set_speed(GPIOPin::Speed::TWO_MHZ);
+
+        pin.Init();
+    }
 }
 
 uint16_t DipSwitch::Read() {
-    return 0;
+    uint16_t result = 0;
+
+    for (unsigned int i = 0; i < pin_count_; i++) {
+        GPIOPin& pin = pins_[i];
+        bool state = !pin.Read();
+        uint16_t bit = state ? 1 : 0;
+        result |= bit << i;
+    }
+
+    return result;
 }
 
 }
