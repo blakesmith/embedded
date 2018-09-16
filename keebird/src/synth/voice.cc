9eb17046deacd320bee721caed141b490359afde
Blake Smith <blakesmith0@gmail.com>
2018-09-16 16:10:17 -0500

    Add initial voice implementation

---
+++ keebird/src/synth/voice.cc
@@ -0,0 +1,43 @@
+#include "voice.h"
+
+namespace synth {
+
+static const uint16_t STARTING_FREQ = 0;
+
+Voice::Voice(const uint32_t sample_rate,
+             const uint32_t control_rate)
+    : osc_({
+              {OscShape::SQUARE, sample_rate, STARTING_FREQ},
+              {OscShape::SIN, sample_rate, STARTING_FREQ}
+          }),
+    envelope_(control_rate, 2550, 1, 20, 0, 225, 0) {
+    uint8_t levels[N_OSC] = {200, 30};
+    mixer_.set_levels(levels, N_OSC);
+    envelope_.Reset();
+}
+
+void Voice::Trigger(const Note* note) {
+    for (size_t j = 0; j < N_OSC; j++) {
+        osc_[j].set_freq(note->frequency);
+    }
+    envelope_.Reset();
+}
+
+void Voice::TickAudio() {
+    for (size_t i = 0; i < N_OSC; i++) {
+        osc_[i].Tick();
+    }
+}
+
+void Voice::TickControl() {
+    envelope_.Tick();
+}
+
+int16_t Voice::Value() {
+    for (size_t i = 0; i < N_OSC; i++) {
+        mixer_.set_input(envelope_.Apply(osc_[i].Value()), i);
+    }
+    return mixer_.Value();
+}
+
+}
---
+++ keebird/src/synth/voice.h
@@ -0,0 +1,35 @@
+#ifndef SYNTH_VOICE_H_
+#define SYNTH_VOICE_H_
+
+#include <cstdint>
+#include <cstddef>
+
+#include "adsr_envelope.h"
+#include "mixer.h"
+#include "note.h"
+#include "osc.h"
+
+namespace synth {
+
+static constexpr size_t N_OSC = 2;
+
+class Voice {
+    Voice(const uint32_t sample_rate,
+          const uint32_t control_rate);
+    ~Voice() = default;
+
+    void Trigger(const Note* note);
+    void TickControl();
+    void TickAudio();
+
+    int16_t Value();
+
+private:
+    Osc osc_[N_OSC];
+    AdsrEnvelope envelope_;
+    Mixer<N_OSC> mixer_;
+};
+
+}
+
+#endif
