6682dcde634f1ee32d88748b67aeb1b39a34d5d9
Blake Smith <blakesmith0@gmail.com>
2019-12-23 12:53:15 -0600

    Make the OK LED blink

---
+++ oxidized_flash/firmware/memory.x
@@ -0,0 +1,6 @@
+MEMORY
+{
+  FLASH (rx) : ORIGIN = 0x00000000, LENGTH = 512K
+  RAM (xrw)  : ORIGIN = 0x20000000, LENGTH = 192K
+}
+_stack_start = ORIGIN(RAM) + LENGTH(RAM);
--- oxidized_flash/firmware/src/main.rs
+++ oxidized_flash/firmware/src/main.rs
@@ -4,13 +4,44 @@
 
 extern crate panic_halt;
 
+extern crate atsamd_hal as hal;
+
 use cortex_m_rt::entry;
+
+use hal::clock::GenericClockController;
+use hal::delay::Delay;
+use hal::gpio::{Floating, GpioExt, Input, Port};
+use hal::prelude::*;
+use hal::target_device as pac;
+use hal::*;
+
+define_pins!(
+    struct Pins,
+    target_device: target_device,
+
+    // OK LED
+    pin a20 = a20,
+);
 
 #[entry]
 fn main() -> ! {
-    let _y;
-    let x = 42;
-    _y = x;
+    let mut peripherals = pac::Peripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
+    let mut clocks = GenericClockController::with_internal_32kosc(
+        peripherals.GCLK,
+        &mut peripherals.MCLK,
+        &mut peripherals.OSC32KCTRL,
+        &mut peripherals.OSCCTRL,
+        &mut peripherals.NVMCTRL,
+    );
+    let mut pins = Pins::new(peripherals.PORT);
+    let mut ok_led = pins.a20.into_open_drain_output(&mut pins.port);
+    let mut delay = Delay::new(core.SYST, &mut clocks);
 
-    loop {}
+    loop {
+        delay.delay_ms(200u8);
+        ok_led.set_high().unwrap();
+        delay.delay_ms(200u8);
+        ok_led.set_low().unwrap();
+    }
 }
