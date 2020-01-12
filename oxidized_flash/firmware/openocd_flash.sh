58bc034a1a779cbe7a7d98cca7e0a331d2dd8718
Blake Smith <blakesmith0@gmail.com>
2020-01-11 20:43:04 -0600

    Initial working board and firmware

--- oxidized_flash/firmware/openocd_flash.sh
+++ oxidized_flash/firmware/openocd_flash.sh
@@ -4,16 +4,16 @@ OPENOCD=openocd
 OPENOCD_CONFIG=$1
 HEX=$2
 
-$OPENOCD -d -f "/usr/local/share/openocd/scripts/interface/jlink.cfg" \
+$OPENOCD -f "/usr/local/share/openocd/scripts/interface/jlink.cfg" \
          -c "transport select swd" \
 	 -c "set CHIPNAME samd51g19a" \
          -f $OPENOCD_CONFIG \
          -c "reset_config none" \
          -c "init" \
          -c "targets" \
+         -c "atsame5 bootloader 0" \
          -c "reset halt" \
          -c "flash write_image $HEX" \
          -c "reset run" \
-         -c "exit"
 
 
