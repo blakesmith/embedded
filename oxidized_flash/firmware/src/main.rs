242c3de35cea2435be153fcb8f746006e13bdddd
Blake Smith <blakesmith0@gmail.com>
2020-01-14 22:44:32 -0600

    Get dotstar LEDs working. Need to figure out why the colors are off

--- oxidized_flash/firmware/src/main.rs
+++ oxidized_flash/firmware/src/main.rs
@@ -17,6 +17,10 @@ use hal::target_device as pac;
 use hal::time::MegaHertz;
 use hal::*;
 
+use apa102_spi::Apa102;
+use smart_leds::SmartLedsWrite;
+use smart_leds_trait::RGB8;
+
 define_pins!(
     struct Pins,
     target_device: target_device,
@@ -50,7 +54,7 @@ fn main() -> ! {
     let apa_ci = pins.apa_ci.into_push_pull_output(&mut pins.port);
     let apa_nc = pins.apa_nc.into_pull_up_input(&mut pins.port);
     let gclk0 = clocks.gclk0();
-    let _spi: SPIMaster2<
+    let spi: SPIMaster2<
         hal::sercom::Sercom2Pad2<gpio::Pa14<gpio::PfC>>,
         hal::sercom::Sercom2Pad0<gpio::Pa12<gpio::PfC>>,
         hal::sercom::Sercom2Pad1<gpio::Pa13<gpio::PfC>>,
@@ -69,10 +73,13 @@ fn main() -> ! {
             apa_ci.into_pad(&mut pins.port),
         ),
     );
+    let mut dotstar = Apa102::new(spi);
 
     let mut delay = Delay::new(core.SYST, &mut clocks);
 
     loop {
+        let colors: [RGB8; 2] = [RGB8 { r: 0, g: 0, b: 64 }, RGB8 { r: 0, g: 64, b: 0 }];
+        dotstar.write(colors.iter().cloned()).unwrap();
         if button_switch.is_low().unwrap() {
             delay.delay_ms(200u8);
             ok_led.set_high().unwrap();
